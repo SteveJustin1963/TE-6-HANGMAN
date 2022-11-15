4eb8c131d35ec852b0f557b0b2af73b94f3a4393
Stephen Justin <stevejustin1963@gmail.com>
2022-11-15 15:50:41 +1100

    Create hang1.c

---
+++ hang1.c
@@ -0,0 +1,120 @@
+ 
+Playing Basic Hangman
+
+Step 1
+Choose one person to be the "host." This is the person that invents the puzzle for the other person to solve. They will be tasked with choosing a word or phrase that "the players" will have to solve.
+The host should be able to spell confidently or the game will be impossible to win.
+step 2
+If you are the host, choose a secret word. The other players will need to guess your word letter by letter, so choose a word you think will be difficult to guess. Difficult words usually have uncommon letters, like "z," or "j," and only a few vowels.
+For longer games, you can also choose phrases.
+
+
+step 3
+Draw a blank line for each letter in the word. For example, if the executioner chooses the word "zipper," she/he would draw six blanks, one for each letter ( _ _ _ _ _ _ ). The host does not tell anyone else the secret word.
+Image titled Play Hangman Step 4
+
+step 4
+Start guessing letters if you are the player. Once the word has been chosen and the players know how many letters in the secret word, begin guessing which letters are in the word by asking the host. For example, you might begin by asking, "is there an 'e' in your word?"
+Generally, start by guessing common letters like vowels, or "s," "t," and "n."
+Image titled Play Hangman Step 5
+
+step 5
+Fill the letter in the blanks if the players guess correctly. Whenever the players guess a letter that is in the secret word, the host fills it into the blank where it occurs. For example, if the word is "zipper" and the players guess "e," then the host will fill in the 5th blank with an "e:" (_ _ _ _ e _).
+
+If the players guess a letter that repeats, fill in both letters. If they guess "p," you would have to fill in both "p"s. ( _ _ p p e _ ).
+
+step 6
+Draw part of the "hangman" when the players guess wrong. Whenever the players guess a letter that is not in the secret word they get a strike that brings them closer to losing. To show this, the host draws a simple stick figure of a man being hung, adding a new part to the drawing with every wrong answer. This is also where you can adjust the difficulty of the game -- the more marks you make, the more wrong guesses the player gets and the easier the game is. The classic order is:
+
+First wrong answer: Draw and upside-down "L." This is the post the man hangs from.
+
+Second: Draw a small circle for the "head" underneath the horizontal line of the "L."
+
+Third: Draw a line down from the bottom of the head for the "body."
+
+Fourth: Draw one arm out from the middle of his body for the "arm."
+
+Fifth: Draw the other arm.
+
+Sixth: Draw one diagonal line from the bottom of the body for the first "leg."
+
+Seventh: Draw the other leg.
+
+Eighth: Connect the head to the post with a "noose." Once you draw the noose the players have lost the game.
+
+step 7
+The players win when they guess the correct word. 
+  If the players get every letter of the word before the host finishes drawing then they win. 
+  At any point a player can try to guess the entire word instead of a single letter, but if they guess the wrong word then the host should treat it as if they guessed a wrong letter.[2]
+=================================
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char secret_word[20];
+    char player_guess;
+    int i, length, chances, flag;
+    
+    printf("Enter the secret word: ");
+    scanf("%s", secret_word);
+    length = strlen(secret_word);
+    
+    char player_guess_word[length];
+    
+    chances = length + 2;
+    
+    for(i=0; i<length; i++)
+    {
+        player_guess_word[i] = '_';
+    }
+    
+    while(chances != 0)
+    {
+        flag = 0;
+        printf("\nEnter your guess: ");
+        scanf(" %c", &player_guess);
+        
+        for(i=0; i<length; i++)
+        {
+            if(secret_word[i] == player_guess)
+            {
+                player_guess_word[i] = player_guess;
+                flag = 1;
+            }
+        }
+        
+        if(flag == 0)
+        {
+            chances--;
+        }
+        
+        printf("\n");
+        
+        for(i=0; i<length; i++)
+        {
+            printf("%c ", player_guess_word[i]);
+        }
+        
+        if(strcmp(secret_word, player_guess_word) == 0)
+        {
+            printf("\nCongratulations, you won the game!\n");
+            break;
+        }
+        
+        printf("\n\nYou have %d chances left.\n", chances);
+    }
+    
+    if(chances == 0)
+    {
+        printf("\nYou lost the game!\n");
+        printf("The secret word was: %s\n", secret_word);
+    }
+    
+    return 0;
+}
